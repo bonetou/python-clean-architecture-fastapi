a228729234e80385d91f61272df65bd6240bb0f7
bonetou <h.boneto@outlook.com>
2023-11-30 08:05:57 -0300

    feat: make isbn a proper value object

--- domain/book/value_objects/isbn.py
+++ domain/book/value_objects/isbn.py
@@ -1,11 +1,14 @@
+from typing import Self
+
+
 class ISBN:
     def __init__(self, isbn: str):
         self._isbn = self._clean_isbn(isbn)
         if not self._is_valid():
-            raise ValueError('Invalid ISBN')
+            raise ValueError("Invalid ISBN")
 
     def _clean_isbn(self, isbn: str):
-        return isbn.replace('-', '').replace(' ', '')
+        return isbn.replace("-", "").replace(" ", "")
 
     def _is_valid(self):
         if not self._isbn.isdigit():
@@ -15,3 +18,10 @@ class ISBN:
             return False
 
         return True
+
+    @property
+    def isbn_code(self):
+        return self._isbn
+
+    def __eq__(self, other: Self):
+        return self.isbn_code == other.isbn_code
--- tests/unit/domain/book/value_objects/test_isbn.py
+++ tests/unit/domain/book/value_objects/test_isbn.py
@@ -7,6 +7,11 @@ def test_valid_isbn_code():
     assert ISBN("978-8595084742")
 
 
+def test_should_return_code_formatted():
+    isbn = ISBN("978-8595084742")
+    assert isbn.isbn_code == "9788595084742"
+
+
 def test_should_be_invalid_when_length_is_not_13():
     with pytest.raises(ValueError, match="Invalid ISBN"):
         ISBN("123456")
@@ -15,3 +20,7 @@ def test_should_be_invalid_when_length_is_not_13():
 def test_should_be_invalid_when_does_not_contain_only_digits():
     with pytest.raises(ValueError, match="Invalid ISBN"):
         ISBN("A78-8595084742")
+
+
+def test_two_isbn_with_same_code_should_be_equal():
+    assert ISBN("978-8595084742") == ISBN("978-8595084742")
